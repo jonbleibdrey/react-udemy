ea0f836dfd01d70ebe1c18d4caa9dbb09d9850e7
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-25 11:42:04 -0500

    added person

---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Person = () => {
+    return (
+        <div>
+           <p>
+            im a Person!
+            </p> 
+        </div>
+    )
+}
+
+export default Person
